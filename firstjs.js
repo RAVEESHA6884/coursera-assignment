32fa9eb7dcae9f5cde600fe08be3712778bba964
Raveesha H <118681307+RAVEESHA6884@users.noreply.github.com>
2023-08-06 15:06:43 +0530

    scripts are uploaded
    simple html and javascript files are uploaded

---
+++ firstjs.js
@@ -0,0 +1,11 @@
+function  add()
+{
+    a=parseInt(prompt("enter the first number"))
+    b=parseInt(prompt("enter the second number"))
+    num=a+b
+    alert(num)
+    alert("Thank you")
+}
+function display(){
+    alert("Thank you for submitting")
+}
